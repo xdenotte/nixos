4e4a32ec08274eb1b5064537074c9f15471dd30d
xdenotte <nottechi999@gmail.com>
2025-08-17 14:48:37 +0200

    Update

--- modules/system.nix
+++ modules/system.nix
@@ -1,4 +1,3 @@
-
 { config, pkgs, ... }:
 
 {
@@ -6,11 +5,11 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  nixpkgs.config.allowUnfree = true;
-  services.power-profiles-daemon.enable = true;
   services.xserver.enable = false;
   services.xserver.videoDrivers = ["nvidia"];
+  nixpkgs.config.allowUnfree = true;
 
+  # NVIDIA + Wayland
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
@@ -21,8 +20,8 @@
     powerManagement.enable = false;
     open = true;
     nvidiaSettings = true;
-
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+
     prime = {
       offload = {
         enable = true;
@@ -33,25 +32,38 @@
     };
   };
 
+  # System cleanup
   nix.gc = {
     automatic = true;
     dates = "daily";
-    options = "--delete-older-than 5d";
+    options = "--delete-older-than 10d";
   };
 
-  systemd = {
-  user.services.polkit-gnome-authentication-agent-1 = {
+  # Services
+  services = {
+    power-profiles-daemon.enable = true;
+    upower.enable = true;
+    timesyncd.enable = true;
+    gvfs.enable = true;
+    displayManager.ly.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+      extraConfig.pipewire."custom-tuning".context.properties."default.clock.min-quantum" = 1024;
+    };
+  };
+
+  # Polkit
+  security.polkit.enable = true;
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
     description = "polkit-gnome-authentication-agent-1";
     wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
     serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
     };
   };
 
@@ -59,132 +71,68 @@
   fonts = {
     enableDefaultPackages = true;
     packages = with pkgs; [
-    fira-sans
-    roboto
-    open-sans
-    noto-fonts
-    inter
-    corefonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-extra
-    lilex
-    noto-fonts-emoji
-    liberation_ttf
-    dejavu_fonts
-    fira-code-symbols
-    material-symbols
-    material-icons
-    wqy_zenhei
-  ];
-};
-
-  # Audio and Some settings
-  services.upower.enable = true;
-  security.polkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    pulse.enable = true;
-    extraConfig = {
-      pipewire = {
-       "bullshit-sound" = {
-         "context.properties" = {
-           "default.clock.min-quantum" = 1024;
-         };
-       };
-      };
-    };
+      fira-sans roboto open-sans inter corefonts lilex
+      noto-fonts noto-fonts-cjk-sans noto-fonts-cjk-serif
+      noto-fonts-extra noto-fonts-emoji liberation_ttf
+      dejavu_fonts fira-code-symbols
+      material-symbols material-icons wqy_zenhei
+    ];
   };
 
+  # Networking
   networking.networkmanager.enable = true;
-  services.timesyncd.enable = true;
-  services.gvfs.enable = true;
 
-  services.displayManager.ly.enable = true;
+  # Icons
   xdg.icons.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    git
-    chromium
-    discord
-    vesktop
-    niri
-    networkmanagerapplet
-    kdePackages.kate
-    fastfetch
-    kitty
-    xwayland-satellite
-    brightnessctl
-    nemo
-    papirus-icon-theme
-    bibata-cursors
-    gnome-themes-extra
-    nwg-look
-    pavucontrol
-    telegram-desktop
-    qt6ct
-    swww
-    wallust
-    cava
-    xdg-user-dirs
-    qt6Packages.qt5compat
-    loupe
-    kdePackages.qtbase
-    kdePackages.qtdeclarative
-    kdePackages.qtstyleplugin-kvantum
-    polkit_gnome
-    hypridle
-    upower
-    heroic
-    prismlauncher
-    spotify
-    power-profiles-daemon
-    mangohud
-    htop
-    windsurf
-    mpv
-    file-roller
-    stress-ng
-    font-manager
-    lshw
-    glib
-    ffmpeg
-    bbe
-    libreoffice
-    gsettings-desktop-schemas
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-ugly
-    gst_all_1.gst-libav
-    gst_all_1.gst-vaapi
-  ];
-
-  programs.appimage = {
-    enable = true;
-    binfmt = true;
-  };
-  programs.niri.enable = true;
-  programs.gamemode.enable = true;
-  programs.steam.enable = true;
-  programs.obs-studio = {
-    enable = true;
-
-    package = (
-      pkgs.obs-studio.override {
-        cudaSupport = true;
-      }
-    );
-
-    plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
-      obs-backgroundremoval
-      obs-pipewire-audio-capture
-      obs-vaapi
-      obs-gstreamer
-      obs-vkcapture
+  # Programs
+  programs = {
+    appimage = {
+      enable = true;
+      binfmt = true;
+    };
+    niri.enable = true;
+    gamemode.enable = true;
+    steam.enable = true;
+    obs-studio = {
+      enable = true;
+      package = pkgs.obs-studio.override { cudaSupport = true; };
+      plugins = with pkgs.obs-studio-plugins; [
+        wlrobs obs-backgroundremoval obs-pipewire-audio-capture
+        obs-vaapi obs-gstreamer obs-vkcapture
       ];
     };
+  };
+
+  # System packages (grouped)
+  environment.systemPackages = with pkgs; [
+
+    # Core / utils
+    git fastfetch htop stress-ng lshw bbe
+
+    # Wayland / WM
+    niri xwayland-satellite hypridle brightnessctl kitty xdg-user-dirs
+
+    # Desktop & theming
+    papirus-icon-theme bibata-cursors gnome-themes-extra nwg-look
+    wallust swww font-manager gsettings-desktop-schemas
+    qt6ct qt6Packages.qt5compat kdePackages.qtbase kdePackages.qtdeclarative
+    kdePackages.qtstyleplugin-kvantum
+
+    # Apps
+    chromium discord vesktop telegram-desktop heroic prismlauncher spotify
+    kdePackages.kate nemo libreoffice file-roller loupe
+
+    # Multimedia
+    mpv cava pavucontrol ffmpeg
+    gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav gst_all_1.gst-vaapi
+
+    # Gaming
+    mangohud
+
+    # Other
+    polkit_gnome windsurf
+  ];
 }
